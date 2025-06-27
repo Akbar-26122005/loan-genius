708dc638193176a513c5c55059467655109211f8
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-06-27 23:19:59 +0500

    Доработана система отслеживания данных сервера

--- src/config/serverClient.js
+++ src/config/serverClient.js
@@ -1,6 +1,14 @@
-const server_address = process.env.SERVER_ADDRESS
+const server_address = process.env.REACT_APP_SERVER_ADDRESS
 
-const getPath = (path = '') =>`${server_address}${path}`
+const getPath = (path = '') => {
+    const full_path = `${server_address}${path}`   
+    console.log({
+        server_address: server_address
+        ,path: path
+        ,full_address: full_path
+    })
+    return full_path
+}
 
 export const getDateFromTimestamp = (timestamp) => new Date(timestamp).toISOString().split('T')[0]
 
