ddb884863bdf752e9fae34dc0090618d40d94f9a
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-06-27 22:55:53 +0500

    Оптимизирована система определения адреса сервера

--- src/config/serverClient.js
+++ src/config/serverClient.js
@@ -1,10 +1,6 @@
-const server = {
-    protocol: process.env.SERVER_PROTOCOL || 'http',
-    domain: process.env.SERVER_ADDRESS || 'localhost:5000',
-}
+const server_address = process.env.SERVER_ADDRESS
 
-const getPath = (path = '') =>`${server.protocol}://${server.domain}${path}`
-// const getPath = (path = '') =>`https://loan-genius-server.vercel.app${path}`
+const getPath = (path = '') =>`${server_address}${path}`
 
 export const getDateFromTimestamp = (timestamp) => new Date(timestamp).toISOString().split('T')[0]
 
